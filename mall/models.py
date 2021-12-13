950d153ceb6248857e9b4c72ebb312fdad5d2264
ji-vvon <jjw9895@duksung.ac.kr>
2021-12-13 16:56:41 +0900

    Product 모델 생성

--- mall/models.py
+++ mall/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=30)
+    info_text = models.CharField(max_length=100, blank=True)
+    # product_img = models.ImageField(upload_to='mall/images/%Y/%m/%d/', blank=True)
+    price = models.CharField(max_length=10)
+    # brand = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    # category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    calories = models.CharField(max_length=10)
+    gram = models.CharField(max_length=10)
+    # nutrition_img = models.ImageField(upload_to='mall/images/%Y/%m/%d/', blank=True)
+
+    def __str__(self):
+        return f'[{self.pk}]{self.product_name} ({self.price}원)'
+
+
