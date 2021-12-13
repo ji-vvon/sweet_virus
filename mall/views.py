5f52ba49e5f484262cfbd91e445ff72dd14f1c13
ji-vvon <jjw9895@duksung.ac.kr>
2021-12-13 23:07:22 +0900

    카테고리 수정

--- mall/views.py
+++ mall/views.py
@@ -22,3 +22,22 @@ class ProductDetail(DetailView):
         context['categories'] = Category.objects.all()
         context['no_category_product_count'] = Product.objects.filter(category=None).count()
         return context
+
+
+def category_page(request, slug):
+    if slug == 'no_category':
+        category = '기타'
+        product_list = Product.objects.filter(category=None)
+    else:
+        category = Category.objects.get(slug=slug)
+        product_list = Product.objects.filter(category=category)
+
+    return render(request,
+                  'mall/product_list.html',
+                  {
+                      'product_list': product_list,
+                      'categories': Category.objects.all(),
+                      'no_category_product_count': Product.objects.filter(category=None).count(),
+                      'category': category
+                  }
+                  )
