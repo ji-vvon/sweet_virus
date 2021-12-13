d0d2739a5d9af2272fd8dcf7dcba7f07a2b8931a
ji-vvon <jjw9895@duksung.ac.kr>
2021-12-13 18:59:23 +0900

    이미지 관리

--- mall/models.py
+++ mall/models.py
@@ -4,13 +4,13 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=30)
     info_text = models.CharField(max_length=100, blank=True)
-    # img = models.ImageField(upload_to='mall/images/%Y/%m/%d/', blank=True)
+    img = models.ImageField(upload_to='mall/images/%Y/%m/%d/', blank=True)
     price = models.CharField(max_length=10)
     # brand = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
     # category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
     calories = models.CharField(max_length=10)
     gram = models.CharField(max_length=10)
-    # nutrition_img = models.ImageField(upload_to='mall/images/%Y/%m/%d/', blank=True)
+    nutrition_img = models.ImageField(upload_to='mall/images/%Y/%m/%d/', blank=True)
 
     def __str__(self):
         return f'[{self.pk}]{self.name} ({self.price}원)'
