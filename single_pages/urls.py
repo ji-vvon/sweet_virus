f2897fdbd44b490f0db6c974be340db50f66db78
ji-vvon <jjw9895@duksung.ac.kr>
2021-12-16 01:32:12 +0900

    대문-최신상품

--- single_pages/urls.py
+++ single_pages/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.landing), #서버IP/아무것도 없음 (대문페이지)
     path('about_us/', views.about_us), #서버IP/about_us/
+    path('mypage/', views.mypage),
 ]
--- single_pages/views.py
+++ single_pages/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-
+from mall.models import Product
 # Create your views here.
 
 def landing(request):
-    return render(request, 'single_pages/landing.html')
+    recent_products = Product.objects.order_by('-pk')[:3]
+    return render(request, 'single_pages/landing.html', {'recent_products': recent_products})
 
 def about_us(request):
     return render(request, 'single_pages/about_us.html')
+
+def mypage(request):
+    return render(request, 'single_pages/mypage.html')
