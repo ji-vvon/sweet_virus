fb3e4a811b83917d35d85e26d4aa8a7ef54b59ed
ji-vvon <jjw9895@duksung.ac.kr>
2021-12-16 20:01:44 +0900

    mall 부분 보완

--- single_pages/views.py
+++ single_pages/views.py
@@ -3,7 +3,7 @@ from mall.models import Product
 # Create your views here.
 
 def landing(request):
-    recent_products = Product.objects.order_by('-pk')[:4]
+    recent_products = Product.objects.order_by('-pk')[:3]
     return render(request, 'single_pages/landing.html', {'recent_products': recent_products})
 
 def about_us(request):
